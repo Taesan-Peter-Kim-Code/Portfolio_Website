fa797f768607c7abeb0140adb8b3b32928ff43e4
Peter <peter@Peters-MacBook-Pro.local>
2023-05-03 13:41:07 -0700

    modified hero

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;' }}>
           <DiCssdeck size="3rem" /> <span>Portfolio</span>
         </a>
       </Link>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Software Engineer <br />
+        Peter Kim
+      </SectionTitle>
+      <SectionText>
+        I'm passionated exploring new technologies and developing software solutions and quick hacks.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://www.linkedin.com/in/peter-kim-436782183/'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
