a41da3e35901eeffdb5816d2d8cf8236e736c189
Peter <peter@Peters-MacBook-Pro.local>
2023-05-03 16:03:19 -0700

    added technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiBackbone, DiCode, DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <br />
+    <br />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web and API development world.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiCode size="3rem" />
+        <ListContainer>
+          <ListTitle>Languages</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python,
+            JavaScript,
+            Java,
+            GoLang
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Django,
+            Node.js,
+            Database
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, Next.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
