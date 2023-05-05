ac0d91c2d85981c41ae9ea21d2055381cfd98c76
Peter <peter@Peters-MacBook-Pro.local>
2023-05-05 13:36:11 -0700

    modified headers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { DiCode, DiCssdeck, DiGhost, DiGitBranch, DiTerminal, DiVisualstudio } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
@@ -9,8 +9,8 @@ const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;' }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a style={{ display: "flex", alignItems: "center", color: "white"}}>
+          <DiGitBranch size="3rem" /> <NavLink>Peter's Portfolio</NavLink>
         </a>
       </Link>
     </Div1>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,11 +8,11 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hi, I'm Software Engineer <br />
-        Peter Kim
+        Welcome <br />
+        I'm Peter Kim
       </SectionTitle>
       <SectionText>
-        I'm passionated exploring new technologies and developing software solutions and quick hacks.
+        Software Engineer who is passionated exploring new technologies and developing software solutions and quick hacks.
       </SectionText>
       <Button onClick={() => window.location = 'https://www.linkedin.com/in/peter-kim-436782183/'}>Learn More</Button>
     </LeftSection>
